d440f6b3d9d5faba854c8c6145ef917487346586
sakuma <sakuma@sakumanoMacBook-Pro.local>
2020-03-08 23:41:35 +0900

    update

---
+++ build/CMakeFiles/VulkanExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanExample.dir/src/main.cpp.o"
+  "VulkanExample"
+  "VulkanExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
